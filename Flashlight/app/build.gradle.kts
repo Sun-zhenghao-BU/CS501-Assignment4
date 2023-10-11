f39a0b4d42f2e21f3d4cd9568ab62bed3b293422
shangzhou Yin <cxysz8246@163.com>
2023-10-11 12:07:46 -0400

    update all

--- Flashlight/app/build.gradle.kts
+++ Flashlight/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.flashlight"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.flashlight"
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.ar.sceneform:filament-android:1.17.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -1,11 +1,125 @@
 package com.example.flashlight
 
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.widget.EditText
+import android.widget.Switch
+import android.view.MotionEvent
+import android.view.View
+import android.hardware.camera2.CameraAccessException
+import android.os.Build
+import android.util.Log
+import android.view.KeyEvent
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var flashlightSwitch: Switch
+    private lateinit var actionEditText: EditText
+    private lateinit var cameraManager: CameraManager
+    private lateinit var gestureDetector: GestureDetector
+    private lateinit var cameraId: String
+    private lateinit var mainLayout: View
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        flashlightSwitch = findViewById(R.id.toggleSwitch)
+        actionEditText = findViewById(R.id.action_bar)
+        val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
+
+        // Initialize the camera to control the flashlight
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+        cameraId = cameraManager.cameraIdList[0]
+
+
+
+        gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(
+                e1: MotionEvent?,
+                e2: MotionEvent,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                Log.d("MyApp", "Entered onFling")
+                if (e1 == null || e2 == null) return false
+                val deltaY = e2.y - e1.y
+                if (Math.abs(deltaY) > 100 && Math.abs(velocityY) > 100) {
+                    if (deltaY < 0) {
+                        flashlightCameraSetting(true)
+                        flashlightSwitch.isChecked = true
+                        Toast.makeText(this@MainActivity, "Turn On", Toast.LENGTH_SHORT).show()
+                    } else {
+                        flashlightCameraSetting(false)
+                        flashlightSwitch.isChecked = false
+                        Toast.makeText(this@MainActivity, "Turn Off", Toast.LENGTH_SHORT).show()
+                    }
+                    return true
+                }
+                return false
+            }
+        })
+
+        mainLayout.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+            true
+        }
+
+
+        // Switch function on/off to turn on/off the flashlight
+        flashlightSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                flashlightCameraSetting(true)
+            } else {
+                flashlightCameraSetting(false)
+            }
+        }
+
+
+
+        actionEditText.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                val actionText = actionEditText.text.toString().trim()
+                if (actionText.equals("ON", ignoreCase = true)) {
+                    flashlightSwitch.isChecked = true
+                    flashlightCameraSetting(true)
+                } else if (actionText.equals("OFF", ignoreCase = true)) {
+                    flashlightSwitch.isChecked = false
+                    flashlightCameraSetting(false)
+                } else {
+                    if (actionText.isNotEmpty()) {
+                        Toast.makeText(this, "Enter only ON/OFF", Toast.LENGTH_LONG).show()
+                    }
+                }
+                return@setOnKeyListener true
+            }
+            false
+        }
+
+
     }
+
+
+
+    // Turn on/off the camera flashlight
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun flashlightCameraSetting(enable: Boolean) {
+        try {
+            if (enable) {
+                cameraManager.setTorchMode(cameraId, true)
+            } else {
+                cameraManager.setTorchMode(cameraId, false)
+            }
+        } catch (e: Exception) {}
+    }
+
+
+
+
 }
